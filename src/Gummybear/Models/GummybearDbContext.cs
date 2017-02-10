f42469af7a4574801497dd651bab6da929090537
Anne Belka <antastic461@gmail.com>
2017-02-10 08:23:37 -0800

    create initial files and set up database migration

---
+++ src/Gummybear/Models/GummybearDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Gummybear.Models
+{
+    public class GummybearDbContext : DbContext
+    {
+        public GummybearDbContext()
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Gummybear;integrated security=True");
+        }
+
+        public GummybearDbContext(DbContextOptions<GummybearDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/Gummybear/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gummybear.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string CountryOfOrigin { get; set; }
+    }
+}
