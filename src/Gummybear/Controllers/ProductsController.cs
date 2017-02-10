dc2c9cb671eaa5afff32bcd2a5f08e6473779fe2
Anne Belka <antastic461@gmail.com>
2017-02-10 14:10:12 -0800

    add Delete route

--- src/Gummybear/Controllers/ProductsController.cs
+++ src/Gummybear/Controllers/ProductsController.cs
@@ -47,5 +47,21 @@ namespace Gummybear.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult Delete(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+
+            return View(thisProduct);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(product => product.ProductId == id);
+            db.Products.Remove(thisProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
